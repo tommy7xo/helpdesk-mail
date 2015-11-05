90972bcca160831d9a928192a73dca3e190f9a6e
Tomasz Bernaciak <t.bernaciak@binarapps.com>
2015-11-05 15:19:50 +0100

    mail config

--- app/services/mail_parser.rb
+++ app/services/mail_parser.rb
@@ -1,29 +1,24 @@
 require 'gmail'
+require 'net/http'
+require 'json'
 class MailParser
 
-  def self.parse_emails(user, pass, url)
+  def self.parse_emails(user, pass, uri)
     gmail = Gmail.new(user, pass) do |gmail|
       puts gmail.inbox.emails(:unread).first
       gmail.inbox.emails(:unread).each do |email|
-        send_ticket(email, url)
+        puts email.to_json
+        url = URI.parse(uri)
+        http = Net::HTTP.new(url.host, url.port)
+        request = Net::HTTP::Post.new(url, {'Content-Type' =>'application/json'})
+        request.body = email.to_json
+        if http.request(request)
+          puts 'true'
+        else
+          puts 'false'
+        end
         email.mark(:read)
       end
     end
   end
-
-  def self.send_ticket(data, url)
-    puts data
-    puts data.to_json
-    url = URI.parse(url)
-    http = Net::HTTP.new(url.host, url.port)
-    #http.use_ssl = true
-    request = Net::HTTP::Post.new(url.path, {'Content-Type' =>'application/json'})
-    request.body = data.to_json
-    if http.request(request)
-      puts 'true'
-    else
-      puts 'false'
-    end
-  end
-
 end
