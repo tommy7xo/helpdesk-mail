c3166e11be8e33da3b82915f32a8a7ea9f46461d
Tomasz Bernaciak <t.bernaciak@binarapps.com>
2015-11-05 15:27:47 +0100

    mail config

--- app/services/mail_parser.rb
+++ app/services/mail_parser.rb
@@ -6,7 +6,7 @@ class MailParser
   def self.parse_emails(user, pass, uri)
     gmail = Gmail.new(user, pass) do |gmail|
       puts gmail.inbox.emails(:unread).first
-      puts gmail.inbox.emails(:unread).first.message
+      puts gmail.inbox.emails(:unread).first.message.to_json
       gmail.inbox.emails(:unread).each do |email|
         puts email.to_json
         url = URI.parse(uri)
