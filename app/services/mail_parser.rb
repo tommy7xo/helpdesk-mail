f174eae5968cc44958b6836803d29011e10b2ece
Tomasz Bernaciak <t.bernaciak@binarapps.com>
2015-11-05 14:47:47 +0100

    mail config

--- app/services/mail_parser.rb
+++ app/services/mail_parser.rb
@@ -3,21 +3,22 @@ class MailParser
 
   def self.parse_emails(user, pass, url)
     gmail = Gmail.new(user, pass)
-    puts gmail.inbox.count
     puts gmail.inbox.count(:unread)
-    gmail.inbox.emails(:unread).each do |message|
-      send(message, url)
-      message.mark(:read)
+    puts gmail.inbox.emails(:unread)
+    puts gmail.inbox.emails(:unread).first
+    gmail.inbox.emails(:unread).each do |email|
+      send(email, url)
+      email.mark(:read)
     end
     gmail.logout
   end
 
-  def send(message, url)
+  def send(data, url)
     url = URI.parse(url)
     http = Net::HTTP.new(url.host, url.port)
     #http.use_ssl = true
     request = Net::HTTP::Post.new(url.path, {'Content-Type' =>'application/json'})
-    request.body = message.to_json
+    request.body = data.to_json
     http.request(request)
   end
 
