47a04bff1d62c69fa4345a746ae8a0fdc94c75f2
Tomasz Bernaciak <t.bernaciak@binarapps.com>
2015-11-06 16:08:30 +0100

    email config

--- app/services/mail_parser.rb
+++ app/services/mail_parser.rb
@@ -6,9 +6,14 @@ class MailParser
   def self.parse_emails(user, pass, uri)
     Gmail.new(user, pass) do |gmail|
       gmail.inbox.emails(:unread).each do |email|
+        from = email.message.from.to_s
+        s[0] = ''
+        s[0] = ''
+        s[-1] = ''
+        s[-1] = ''
         ticket = {
           ticket: {
-            from:     email.message.from.to_s,
+            from:     from,
             subject:  email.message.subject.to_s,
             content:  email.message.body.to_s
           }
@@ -16,7 +21,6 @@ class MailParser
         url = URI.parse(uri)
         http = Net::HTTP.new(url.host, url.port)
         request = Net::HTTP::Post.new(url, {'Content-Type' => 'application/json'})
-        request.content_type = 'application/json'
         request.body = ticket.to_json
         http.request(request)
 
