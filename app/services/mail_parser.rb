12d1674a47d28e57241d709af4a70f12a3c35129
Tomasz Bernaciak <t.bernaciak@binarapps.com>
2015-11-09 08:56:36 +0100

    email config

--- app/services/mail_parser.rb
+++ app/services/mail_parser.rb
@@ -6,28 +6,29 @@ class MailParser
   def self.parse_emails(user, pass, uri)
     Gmail.new(user, pass) do |gmail|
       gmail.inbox.emails(:unread).each do |email|
-        from = email.message.from.to_s
-        # delete some shit from email
-        from[0] = ''
-        from[0] = ''
-        from[-1] = ''
-        from[-1] = ''
-
         ticket = {
           ticket: {
-            from:     from,
+            from:     mail_format( email.message.from.to_s ),
             subject:  email.message.subject.to_s,
             content:  email.message.body.to_s
           }
         }
-        url = URI.parse(uri)
-        http = Net::HTTP.new(url.host, url.port)
-        request = Net::HTTP::Post.new(url, {'Content-Type' => 'application/json'})
-        request.body = ticket.to_json
-        http.request(request)
-
+        request(ticket, uri)
         email.mark(:read)
       end
     end
+  end
+
+  def self.request(message, uri)
+    url = URI.parse(uri)
+    http = Net::HTTP.new(url.host, url.port)
+    request = Net::HTTP::Post.new(url, {'Content-Type' => 'application/json'})
+    request.body = message.to_json
+    http.request(request)
+  end
+
+  def self.mail_format(string)
+    string[0..1] = ''
+    string[-2..-1] = ''
   end
 end
